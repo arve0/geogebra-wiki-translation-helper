dff88a6cc1a6d175c2148ea8063cd948d132e277
arve0 <arve.seljebu@gmail.com>
2014-06-22 23:41:48 +0200

    rewrite cache to class

--- cache.py
+++ cache.py
@@ -15,133 +15,205 @@ import pywikibot
 import pickle
 import config
 import sys
+import re
 
 
-def _filename(namespace=0, language='en'):
+class PagesCache(object):
     """
-    Returns filename for pickle
+    Abstraction for wikipages, from wiki or pickle cache.
+
+    Raises NameError if namespace not found in wiki or language not found
+    in config.py.
     """
+    def __init__(self, namespace='Manual', language='en'):
+        self.namespace = namespace.lower().capitalize()
+        self.language = language.lower()
+        self._check_language()
+        self._site = pywikibot.Site(code=language, fam='geogebra')
+        self.namespace_number = self._get_namespace_number()
+        self.filename = self._get_filename()
+        self.pages = None
 
-    return 'data/pages-' + str(namespace) + '-' + language + '.pickle'
+
+    def get_from_wiki(self):
+        """
+        Retrieve all pages from specified namespace, save to pickle
+        (data/pages-namespace-languagecode.pickle) and return list of pages.
+
+        Raises IOError upon write error.
+        """
 
 
-def _check_params(namespace, language):
-    """
-    Checks if params is OK.
-    """
+        pages = pagegenerators.AllpagesPageGenerator(
+            site=self._site, namespace=self.namespace_number, content=True)
 
-    lang_ok = False
-    namespace = True # TODO
-    for lang in config.LANGUAGES:
-        if lang.code == language:
-            lang_ok = True
+        self.pages = []
+        print 'Getting pages from wiki in namespace ' + self.namespace + \
+                ' and language ' + self.language
+        for page in pages:
+            obj = {
+                'id': page._pageid,
+                'title': self._strip_namespace(page.title()),
+                'text': page.text,
+                'timestamp': page._timestamp, # useful?
+                'revid': page._revid,
+                'editTime': page.editTime(),
+                'namespace': page.namespace(),
+                'isRedirect': page._isredir,
+            }
+            self.pages.append(obj)
 
-    if not lang_ok: # TODO namespace
-        print 'Language ' + language + ' not in config. Please add it to config.py'
+        print 'Saving pages to ' + self.filename
+        file_ = open(self.filename, 'w')
+        pickle.dump(self.pages, file_, -1)
+        file_.close()
 
-    return lang_ok and namespace
+        return self.pages
 
+
+    def _strip_namespace(self, title):
+        """
+        Returns title without Namespace:
+        """
+
+        # Special case, no namespace
+        if self.namespace_number == 0:
+            return title
+
+        regex = re.compile('^{0}:'.format(self.namespace))
+        title = re.sub(regex, '', title)
+        return title
+
+
+    def get_from_cache(self):
+        """
+        Returns list of pages from pickle cache self.filename.
+
+        Raises IOError upon file not found.
+        """
+
+        print 'Reading pages from ' + self.filename
+        file_ = open(self.filename)
+        self.pages = pickle.load(file_)
+        file_.close()
+
+        return self.pages
+
+
+    def _get_filename(self):
+        """
+        Returns filename for pickle
+        """
+
+        filename = 'data/pages-{0}-{1}.pickle'\
+                .format(self.namespace_number, self.language)
+        return filename
+
+
+    def _get_namespace_number(self):
+        """
+        Sets integer for self.namespace to self.namespace_number.
+
+        Raises NameError if not found.
+        """
+
+        # Special case - no namespace, lets call it main
+        if self.namespace == 'Main':
+            namespace = ''
+        else:
+            namespace = self.namespace
+
+        namespace_number = self._site.ns_index(namespace)
+        if namespace_number == None:
+            raise NameError('ERROR: Namespace {0} not'.format(self.namespace) +\
+                    ' found in "{0}" wiki.'.format(self.language))
+
+        return namespace_number
+
+
+    def _check_language(self):
+        """
+        Checks if language is in config.py. Raises NameError if not found.
+
+        Raises NameError if not found.
+        """
+
+        lang_ok = False
+        for lang in config.LANGUAGES:
+            if lang.code == self.language:
+                lang_ok = True
+
+        if not lang_ok:
+            raise NameError('ERROR: Language ' + self.language + \
+                    ' not in config. Please add it to config.py')
+
+
+    def print_(self):
+        """
+        Prints all page titles.
+        """
+
+        msg = 'Pages in cache'
+        print msg
+        print '='*len(msg)
+
+        for page in self.pages:
+            # Print title + beginning of text (strip until ;)
+            print page['title'] + ':'
+            start = page['text'].find(';')
+            if start == -1:
+                # match line starting with A-Z, * or :
+                # match # (redirects)
+                # match {{lowercase
+                match = re.search('(\n[A-Z*:]|#|\{\{[a-z])', page['text'])
+                if match == None:
+                    start = 0
+                else:
+                    start = match.span()[0]
+            print page['text'][start:start+80].replace('\n', ' ')
+
+# for running as a script
 def _print_usage():
     """
     Prints usage of this file as a script.
     """
 
-    print 'Usage:'
-    print sys.argv[0] + '\n'
-    print 'get [namespace] [language] \t- get all pages from wiki and store to pickle'
-    print 'show [namespace] [language] \t- print all page titles from cache\n'
-    print 'Default values:'
-    print 'namespace: 0'
-    print 'language: en'
+    usage = '''
+    Usage:
+    {0} command [language] [namespace]
 
+    Commands:
+    get \t- get all pages from wiki and store to pickle
+    show \t- print all page titles from cache
 
-def _call(function, args):
-    """
-    Helper function to call function with right amount of args.
-    """
-
-    if len(args) == 4:
-        function(int(args[2]), args[3])
-    elif len(args) == 3:
-        function(int(args[2]))
-    elif len(args) == 2:
-        function()
-    else:
-        _print_usage()
-
+    Default values:
+    language: en (language.code in config.py)
+    namespace: manual
     
-def get_from_wiki(namespace=0, language='en'):
+    To get default/empty namespace, use main. Ex: {0} show main
+    '''.format(sys.argv[0])
+
+    print usage
+    sys.exit()
+
+
+def _setup_pages(args):
     """
-    Retrieve all pages from specified namespace, save to pickle
-    (data/pages-namespace-languagecode.pickle) and return list of pages.
+    Helper function to set up PageCache object.
     """
 
-    if not _check_params(namespace, language):
-        return
-
-    site = pywikibot.Site(language, fam='geogebra')
-    pages = pagegenerators.AllpagesPageGenerator(site=site, namespace=namespace, content=True)
-
-    pagestore = []
-    print 'Getting pages from wiki in namespace ' + str(namespace) + ' and language ' + language
-    for page in pages:
-        obj = {
-            'id': page._pageid,
-            'title': page.title(),
-            'text': page.text,
-            'timestamp': page._timestamp, # useful?
-            'revid': page._revid,
-            'editTime': page.editTime(),
-            'namespace': page.namespace(),
-            'isRedirect': page._isredir,
-        }
-        pagestore.append(obj)
-
-    filename = _filename(namespace, language)
-    print 'Saving pages to ' + filename
-    try:
-        file_ = open(filename, 'w')
-        pickle.dump(pagestore, file_, -1)
-        file_.close()
-    except IOError as (errno, strerror):
-        print 'I/O error({0}): {1}'.format(errno, strerror)
-    except ValueError:
-        print 'Could not convert data to an integer.'
-    except:
-        print 'Unexpected error:', sys.exc_info()[0]
-        raise
+    pages = None
+    if len(args) == 4:
+        pages = PagesCache(language=args[2], namespace=args[3])
+    elif len(args) == 3:
+        pages = PagesCache(language=args[2])
+    elif len(args) == 2:
+        pages = PagesCache()
+    elif len(args) > 5:
+        raise Exception('ERROR: Wrong number of arguments. Got ' + \
+                '{0}, expected 1-3.'.format(len(args)-1))
 
     return pages
-
-
-def get_from_cache(namespace=0, language='en'):
-    """
-    Returns list of pages from pickle cache.
-    """
-
-    if not _check_params(namespace, language):
-        return
-
-    filename = _filename(namespace, language)
-    file_ = open(filename)
-    pages = pickle.load(file_)
-    file_.close()
-
-    return pages
-
-
-def print_pickle(*args):
-    """
-    Prints all page titles in pickle.
-    """
-
-    msg = 'Pages in cache'
-    print msg
-    print '='*len(msg)
-
-    pages = get_from_cache(*args)
-    for page in pages:
-        print page['title']
 
 
 def main(args):
@@ -149,12 +221,24 @@ def main(args):
     If called as script.
     """
 
-    if 'get' in args:
-        _call(get_from_wiki, args)
-    elif 'show' in args:
-        _call(print_pickle, args)
-    else:
-        _print_usage()
+    try:
+        pages = _setup_pages(args)
+
+        # does not catch all, but is sufficient
+        if 'get' in args:
+            pages.get_from_wiki()
+        elif 'show' in args:
+            pages.get_from_cache()
+            pages.print_()
+        else:
+            _print_usage()
+    except IOError as error:
+        print 'I/O ERROR: {0}'.format(error)
+    except NameError as error:
+        print format(error)
+    except Exception as error:
+        print format(error)
+        raise # raise exception for debugging
 
 
 if __name__ == '__main__':
