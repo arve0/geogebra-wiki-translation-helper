18d8bec50aba09b17b3dd8bb8426d86849b03155
arve0 <arve.seljebu@gmail.com>
2014-07-06 13:30:06 +0200

    command to list namespaces

---
+++ namespaces.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: namespaces.py
+Author: Arve Seljebu
+Email: arve.seljebu@gmail.com
+Github: arve0
+Description: List all namespaces in given language.
+"""
+
+from pywikibot import Site
+import sys
+from language import Language
+
+if __name__ == '__main__':
+    """Print all namespaces in given language to console."""
+    length = len(sys.argv)
+    if length > 2:
+        print_usage()
+        sys.exit()
+    elif length == 1:
+        language = Language('en')
+    else:
+        language = Language(sys.argv[1])
+
+    site = Site(language.code, 'geogebra')
+    print u'== Namespaces in {} =='.format(language)
+    for number, names in site.namespaces().iteritems():
+        names_print = ', '.join(names)
+        print u'Number: {0}\t Names: {1}'.format(number, names_print)
